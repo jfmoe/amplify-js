e99b5e165499bcd46b909807a99ef572ad229092
鸣上 <mingshang.tao@tuya.com>
2024-07-08 16:31:11 +0800

    fix:Import NetInfo from react-native

--- packages/pubsub/src/utils/ReachabilityMonitor/index.native.ts
+++ packages/pubsub/src/utils/ReachabilityMonitor/index.native.ts
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import { Reachability } from '@aws-amplify/core';
-import { default as NetInfo } from '@react-native-community/netinfo';
+import { NetInfo } from 'react-native';
 
 export const ReachabilityMonitor = () =>
 	new Reachability().networkMonitor(NetInfo);
